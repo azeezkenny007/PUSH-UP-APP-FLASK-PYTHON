17c9d7dcfe6e26653d1b4ef2d29a3ea841794128
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-07 04:26:07 +0100

    feat:Item Model created

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column,String,Integer
 
 app = Flask(__name__)
-app.debug =True
+# path for the database stored
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' 
+# To track modifications 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+# model created for the Item database
+class Item(db.Model):
+    id = Column(Integer,primary_key=True)
+    name =Column(String(30),nullable=True,unique =True)
+    price =Column(Integer,nullable=True)
+    barcode = Column(String(12),nullable=False,unique=True)
+    description = Column(String(1024),nullable=False,unique=True)
 
 @app.route('/')
 @app.route("/home")
