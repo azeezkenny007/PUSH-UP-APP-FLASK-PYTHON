b2c635cf9d7b37e73e8f948e5d75902101dafa38
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-07 03:50:31 +0100

    feat:used the table Sqlalchemy to create  a db

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug =True
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route("/home")
