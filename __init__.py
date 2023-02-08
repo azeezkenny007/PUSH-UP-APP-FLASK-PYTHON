3f56d7732a8769fa052a70ce2378fb8e2d94322a
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-08 11:02:13 +0100

    feat:change model

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 db =SQLAlchemy()
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"]="secret-key"
-    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db.sqlite"
+    # app.config["SECRET_KEY"]="secret-key"
+    # app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///db.sqlite"
     
     db.init_app(app)
    
---
+++ models.py
@@ -0,0 +1,8 @@
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
