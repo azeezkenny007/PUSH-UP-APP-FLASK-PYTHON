b66711c6a8643f9c35b99c8ec50e451619d41c5c
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-09 13:08:05 +0100

    feat:modifications

--- models.py
+++ models.py
@@ -3,11 +3,11 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 
-# class User(db.Model):
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String(100))
-#     email = Column(String(100), unique=True)
-#     password = Column(String(100))
+class UserOne(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
 
 
