1b5e345c4180f5e11474fee3a8673ad68570a6b5
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-10 01:31:53 +0100

    feat:visualize data using db sqlite

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, request, url_for
 from werkzeug.security import generate_password_hash
+from .models import User,session
 
 auth = Blueprint("auth", __name__)
 
@@ -25,7 +26,14 @@ def signup_post():
     name = request.form.get("name")
     password = request.form.get("password")
 
-    print(email, name, password)
+    # print(email, name, password)
+    user =session.query(User).filter_by(email=email).first()
+    if user:
+        print("User already exists")
+    else:
+        new_user = User(email=email,name=name,password=generate_password_hash(password,method="sha256"))
+        session.add(new_user)
+        session.commit()
 
     return redirect(url_for("auth.login"))
 
--- models.py
+++ models.py
@@ -13,9 +13,10 @@ class User(Base):
     email = Column("email",String(120), unique=True)
     password=Column("password",String(120))
 
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, email=None, password=None):
         self.name = name
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return f'<User {self.name!r}>'
