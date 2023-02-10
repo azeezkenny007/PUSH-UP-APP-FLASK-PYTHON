d93e04dbd93c4a0631f8187f11a8a129f8d35f02
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-02-10 01:52:48 +0100

    install:flask-login

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-
 from sqlalchemy import Column, Integer, String,create_engine
 from sqlalchemy.orm import sessionmaker,relationship
+from flask
 
 from sqlalchemy.ext.declarative import declarative_base
 
